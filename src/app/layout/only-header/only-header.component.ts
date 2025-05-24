dffc55786ce1d5dd29f8cbf05ba57d094da90ad7
Prakash <dpsrkp.admission@mobilise.co.in>
2025-05-24 15:22:11 +0530

    first page

--- src/app/layout/only-header/only-header.component.ts
+++ src/app/layout/only-header/only-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { Header2Component } from "../../shared/components/header2/header2.component";
 
 @Component({
   selector: 'app-only-header',
   standalone: true,
-  imports: [HeaderComponent,  RouterModule],
+  imports: [RouterModule, Header2Component],
   templateUrl: './only-header.component.html',
   styleUrl: './only-header.component.scss'
 })
--- src/app/modules/onboarding/vendor-onboard/vendor-onboard.component.ts
+++ src/app/modules/onboarding/vendor-onboard/vendor-onboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-vendor-onboard',
@@ -7,6 +10,32 @@ import { Component } from '@angular/core';
   templateUrl: './vendor-onboard.component.html',
   styleUrl: './vendor-onboard.component.scss'
 })
-export class VendorOnboardComponent {
 
+export class VendorOnboardComponent {
+  tabIndex: number = 0;
+  
+
+  constructor(private router: Router) {}
+
+  setTab(index: number) {
+    this.tabIndex = index;
+  }
+
+nextTab() {
+  if (this.tabIndex < 3) this.tabIndex++;
+}
+
+prevTab() {
+  if (this.tabIndex > 0) this.tabIndex--;
+}
+resetStepper() {
+  this.tabIndex = 0;
+}
+
+
+  
+  submitForm() {
+    console.log('Form submitted!');
+    this.router.navigate(['/all-courses'])
+  }
 }
