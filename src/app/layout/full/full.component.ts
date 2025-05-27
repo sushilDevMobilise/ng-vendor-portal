6253445c45e92ad0c782528d82922d4dd6390bad
Mukul saini <mobilise.temp7@mobilise.co.in>
2025-05-27 17:01:26 +0530

    Dashboard page work completed

--- src/app/layout/full/full.component.ts
+++ src/app/layout/full/full.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-full',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, RouterModule],
+  imports: [HeaderComponent, SidebarComponent, RouterModule, FooterComponent],
   templateUrl: './full.component.html',
   styleUrl: './full.component.scss'
 })
